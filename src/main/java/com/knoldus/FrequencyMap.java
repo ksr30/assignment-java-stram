835de4e7ea337fd195cd0e33f358f899da625cd5
Krishna Singh <krishna.singh9926@gmail.com>
2020-03-05 10:48:24 +0530

    Updated Changes

--- src/main/java/com/knoldus/FrequencyMap.java
+++ src/main/java/com/knoldus/FrequencyMap.java
@@ -3,6 +3,7 @@ package com.knoldus;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -23,7 +24,7 @@ class FileReader {
     Stream<String> lineFinder() throws IOException {
 
 
-        String fileName = "/home/knoldus/Documents/assignment-java-stream/src/main/resources/file";
+        String fileName = "./src/main/resources/file";
 
         //read file into stream, try-with-resources
 
@@ -37,14 +38,13 @@ class FileReader {
 
 public class FrequencyMap {
     public static void main(String[] args) throws IOException {
-        FileReader readOpration=new FileReader();
-        FrequencyMapFinder modifyOpeartion=new FrequencyMapFinder();
-        Stream<String> streamOflines=readOpration.lineFinder();
-        List<String> totalWords = streamOflines.map(ele->ele.toString()).map(line->line.split(" ")).map(words->words.toString()).collect(Collectors.toList());
-        System.out.println(totalWords);
-    //    System.out.println(modifyOpeartion.frequencyFinder(totalWords));
+        FileReader readOpration = new FileReader();
+        FrequencyMapFinder modifyOpeartion = new FrequencyMapFinder();
+        Stream<String> streamOflines = readOpration.lineFinder();
 
+        Stream<String> totalWords = streamOflines.map(line -> line.split(" ")).flatMap(Arrays::stream);
 
+        System.out.println(modifyOpeartion.frequencyFinder(totalWords));
 
 
     }
